662f7694deeba50126a833466763d42fec10307f
pvpatil5 <pvpatil123@yahoo.com>
2020-10-08 12:11:50 +0530

    class 3 added and Class 1 Modified

--- src/pac/Class1.java
+++ src/pac/Class1.java
@@ -3,7 +3,7 @@ package pac;
 public class Class1 {
 
 	public static void main(String[] args) {
-	System.out.println("Class 1 Created");
+	System.out.println("Class 1 Created and Made changes");
 
 	}
 
---
+++ src/pac/Class3.java
@@ -0,0 +1,10 @@
+package pac;
+
+public class Class3 {
+
+	public static void main(String[] args) {
+		System.out.println("Class 3 Added");
+
+	}
+
+}
